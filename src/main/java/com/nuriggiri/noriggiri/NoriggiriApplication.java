dc09ad423cf1e2c98e55c4259337ea296ec472b0
Irus0-0 <ikaros9313@gmail.com>
2021-05-31 17:45:46 +0900

    first 프로젝트 폴더만 있음

---
+++ src/main/java/com/nuriggiri/noriggiri/NoriggiriApplication.java
@@ -0,0 +1,13 @@
+package com.nuriggiri.noriggiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoriggiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoriggiriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuriggiri/noriggiri/NoriggiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuriggiri.noriggiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoriggiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
