ae8bf5577a9bff5fa05d8371fcd291f17261f430
Irus0-0 <ikaros9313@gmail.com>
2021-05-31 18:09:45 +0900

    서버구동 완료

--- src/main/java/com/nuriggiri/nuriggiri/ServletInitializer.java
+++ src/main/java/com/nuriggiri/nuriggiri/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.nuriggiri.noriggiri;
+package com.nuriggiri.nuriggiri;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- src/test/java/com/nuriggiri/nuriggiri/NoriggiriApplicationTests.java
+++ src/test/java/com/nuriggiri/nuriggiri/NoriggiriApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nuriggiri.noriggiri;
+package com.nuriggiri.nuriggiri;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
