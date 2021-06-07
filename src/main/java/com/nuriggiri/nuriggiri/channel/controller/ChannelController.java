180c3c8f24f38166b13a3b8504c1f4b31d7305ea
ssong6735 <because6735@gmail.com>
2021-06-08 05:44:21 +0900

    [21.06.08] # channel 기능 추가중

---
+++ src/main/java/com/nuriggiri/nuriggiri/channel/controller/ChannelController.java
@@ -0,0 +1,108 @@
+package com.nuriggiri.nuriggiri.channel.controller;
+
+import com.nuriggiri.nuriggiri.board.service.BoardService;
+import com.nuriggiri.nuriggiri.channel.domain.Channel;
+import com.nuriggiri.nuriggiri.channel.domain.ModifyChannel;
+import com.nuriggiri.nuriggiri.channel.service.ChannelService;
+import com.nuriggiri.nuriggiri.user.domain.User;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@Log4j2
+@CrossOrigin
+@RequestMapping("/channel")
+public class ChannelController {
+
+    //서비스 파일과 연결
+    public final ChannelService channelService;
+    @Autowired
+    public ChannelController(ChannelService channelService) {
+        this.channelService = channelService;
+    }
+
+    //채널 목록 가져오기
+    @GetMapping("/channel")
+    public String viewList(Model model) {
+        List<Channel> channelList = channelService.viewList();
+        model.addAttribute("list", channelList);
+        return "/channel";
+    }
+
+    //채널 생성 화면 요청
+    @GetMapping("/addCh")
+    public String create() {
+        return "/channel/addCh";
+    }
+
+    //채널 생성 처리 요청
+    @PostMapping("/addCh")
+    public String create(Channel channel) {
+        try {
+            channelService.create(channel);
+        } catch (Exception e) {
+            return "/channel/addCh";
+        }
+        return "redirect:channel";
+    }
+
+    //채널 정보 상세보기 요청
+    @GetMapping("/viewCh")
+    public String viewInfo(int channelNo, Model model) {
+        Channel content = channelService.viewInfo(channelNo);
+        model.addAttribute("channel", content);
+        return "/channel/viewCh";
+    }
+
+    //채널 수정 화면 요청
+    @GetMapping("/modCh")
+    public String update(int channelNo, Model model) {
+        model.addAttribute("channel", channelService.viewInfo(channelNo));
+        return "/channel/modCh";
+    }
+
+    //채널 수정 처리 요청
+    @PostMapping("/modCh")
+    public String update(ModifyChannel modifyChannel) {
+        // 원본데이터를 찾아서 수정데이터로 변경하는 로직(서비스에 넣어주는게 좋다)
+        Channel channel = channelService.viewInfo(modifyChannel.getChannelNo());
+        channel.setChannelName(modifyChannel.getChannelName());
+        channel.setChannelInfo(modifyChannel.getChannelInfo());
+        channel.setChannelPw(modifyChannel.getChannelPw());
+        try {
+            channelService.update(channel);
+        } catch (Exception e) {
+            return "redirect:/channel/modCh?channelNo=" + modifyChannel.getChannelNo();
+        }
+        return "redirect:/channel/viewCh?channelNo=" + modifyChannel.getChannelNo();
+    }
+
+    //채널 삭제 요청
+    @GetMapping("/delete")
+    public String delete(int channelNo) {
+        channelService.delete(channelNo);
+        return "redirect:/channel";
+    }
+
+
+    //채널 접속
+    //void join(int channelNo, String channelPw, User userNo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
