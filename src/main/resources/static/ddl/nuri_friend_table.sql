790c5576210b09ab0f65882a456425b98abc3c91
Irus0-0 <ikaros9313@gmail.com>
2021-06-10 16:40:01 +0900

    [21-06-10] 수정

--- src/main/resources/static/ddl/nuri_friend_table.sql
+++ src/main/resources/static/ddl/nuri_friend_table.sql
@@ -1,7 +1,6 @@
 CREATE TABLE nuri_friend(
     user_no NUMBER(10), --유저번호 PK FK
     target_no NUMBER(10), -- 친구 타겟 유저번호 FK - 유저번호
-    relationship VARCHAR2(20), --친구 관계
+    relationship VARCHAR2(20) --친구 관계
 
-    CONSTRAINT pk_nuri_friend PRIMARY KEY (user_no)
 );
